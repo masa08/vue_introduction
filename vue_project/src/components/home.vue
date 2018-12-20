aa1117337956b92b07bb2c0fdc58667b53126f5b
Masa08 <ushimasarug08@gmail.com>
2018-12-21 02:02:02 +0900

    Deploy setting in github

---
+++ vue_project/src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="main-image">
+      <h1 class="main-image-title mb-5">
+        Hello World! <span style="font-size: 50px;">by <span style="color:rgba(65,184,131);">masa08</span></span>
+      </h1>
+      <p style="color:gray;">はじめまして、masa08です。<br>このサイトはmasa08の自己紹介サイトです。</p>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.main-image-title {
+  font-size: 70px;
+  padding-top: 250px;
+}
+@media screen and (max-width:800px){
+  .main-image-title {
+  font-size: 50px;
+  padding-top: 150px;
+  }
+}
+</style>
---
+++ vue_project/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import home from '@/components/home'
+import introduction from '@/components/introduction'
+import language from '@/components/language'
+import intern from '@/components/intern'
+import portfolio from '@/components/portfolio'
+import contact from '@/components/contact'
+
+// import Bootstrap
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(Router)
+Vue.use(BootstrapVue)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: home
+    },
+    {
+      path: '/introduction',
+      component: introduction
+    },
+    {
+      path: '/language',
+      component: language
+    },
+    {
+      path: '/intern',
+      component: intern
+    },
+    {
+      path: '/portfolio',
+      component: portfolio
+    },
+    {
+      path: '/contact',
+      component: contact
+    }
+  ]
+})
